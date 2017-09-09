8900183c5f97ce9386917b267081e1a8255a38c8
MSDN-SPECIAL\Administrator <sora8324@naver.com>
2017-09-09 13:14:35 +0900

    no message

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    
+    $(".owl-carousel").owlCarousel({
+        
+        item : 1,
+        margin : 0,
+        loop : true
+
+        
+        
+
+
+
+
+    });
+    
+});
