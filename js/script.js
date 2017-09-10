9db5ebc9f5cd12a087518f75971a41358f58f915
MSDN-SPECIAL\Administrator <sora8324@naver.com>
2017-09-10 13:09:11 +0900

    image slider with swiper add

--- js/script.js
+++ js/script.js
@@ -5,12 +5,12 @@ $(document).ready(function(){
         items : 1,
         margin : 0,
         loop : true
-
-        
-        
-
-
-
-
     });
+        var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        slidesPerView: 4,
+        paginationClickable: true,
+        spaceBetween: 30,
+    });
+    
 });
